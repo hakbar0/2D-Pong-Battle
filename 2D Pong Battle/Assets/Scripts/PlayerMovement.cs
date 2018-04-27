ab24b142434c04ff551bc736cb8d725f65c2a007
Haseeb Akbar <haseeb2000@hotmail.com>
2018-04-27 18:37:06 +0100

    Able to move player and restrict the player in one area.

---
+++ 2D Pong Battle/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Awake () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveVertical = Input.GetAxis ("Vertical");
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+}
